666d6cb61547a150a0c58eac749739dcb593bc38
Chrisdocs <chriswolfe.dev@gmail.com>
2022-07-07 17:25:41 -0500

    changed api unit delivery to imperial

--- components/ForecastCard.js
+++ components/ForecastCard.js
@@ -4,20 +4,9 @@ import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
 export default function ForecastCard({ props }) {
-
-  // set props to variables
-	console.log(props);
-
-
   return (
     <div>
-      {props === null ? (
-        <div>No Forecast Data Available</div>
-      ) : (
-        <div className={styles.card}>
-					hi
-        </div>
-      )}
+      {props === null ? <div>No Forecast Data Available</div> : <div>{props.list[0].main.temp}</div>}
     </div>
   );
 }
--- pages/api/ForecastAPI.js
+++ pages/api/ForecastAPI.js
@@ -4,7 +4,7 @@ export async function getServerSidePropF(lat, lon){
 	const LON = lon;
 	const API_KEY = process.env.API_KEY;
 
-	const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${API_KEY}`);
+	const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${API_KEY}&units=imperial`);
 	const data = await response.json();
 	if (!data) {
 		return {
