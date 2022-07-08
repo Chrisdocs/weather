69e2deb8eb1566935f06caad3adb32db40286ba3
Chrisdocs <chriswolfe.dev@gmail.com>
2022-07-08 13:03:14 -0500

    scss structure for forecast

--- components/ForecastCard.js
+++ components/ForecastCard.js
@@ -34,22 +34,22 @@ export default function ForecastCard({ props }) {
 
 	if (isData === true) {
 		return (
-      <div>
-        <div className={styles.day1}>
-          <p>{`${dt}`}</p>
-          <p className={styles.zero}>12:00am: {`${arr[0].main.temp}`}</p>
-          <p className={styles.three}>3:00am: {`${arr[1].main.temp}`}</p>
-          <p className={styles.six}>6:00am: {`${arr[2].main.temp}`}</p>
-          <p className={styles.nine}>9:00am: {`${arr[3].main.temp}`}</p>
-          <p className={styles.twelve}>12:00pm: {`${arr[4].main.temp}`}</p>
-          <p className={styles.fifteen}>3:00pm: {`${arr[5].main.temp}`}</p>
-          <p className={styles.eighteen}>6:00pm: {`${arr[6].main.temp}`}</p>
-          <p className={styles.twentyone}>9:00pm: {`${arr[7].main.temp}`}</p>
+      <div className={styles.forecastContainer}>
+          <p className={styles.dt}>{`${dt}`}</p>
+        <div className={styles.day}>
+          <p className={styles.zero}>12am: {`${arr[0].main.temp}`}</p>
+          <p className={styles.three}>3am: {`${arr[1].main.temp}`}</p>
+          <p className={styles.six}>6am: {`${arr[2].main.temp}`}</p>
+          <p className={styles.nine}>9am: {`${arr[3].main.temp}`}</p>
+          <p className={styles.twelve}>12pm: {`${arr[4].main.temp}`}</p>
+          <p className={styles.fifteen}>3pm: {`${arr[5].main.temp}`}</p>
+          <p className={styles.eighteen}>6pm: {`${arr[6].main.temp}`}</p>
+          <p className={styles.twentyone}>9pm: {`${arr[7].main.temp}`}</p>
         </div>
-        <div className={styles.day2}></div>
-        <div className={styles.day3}></div>
-        <div className={styles.day4}></div>
-        <div className={styles.day5}></div>
+        <div className={styles.day}></div>
+        <div className={styles.day}></div>
+        <div className={styles.day}></div>
+        <div className={styles.day}></div>
       </div>
     );
 	} else {
